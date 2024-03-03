15acf8dbae716856a746278612c07283b6ce91f5
Tooru762 <156686008+Tooru762@users.noreply.github.com>
2024-03-03 10:53:37 +0400

    Add files via upload

---
+++ day4/ok.py
@@ -0,0 +1,7 @@
+name=input("whats your name:") 
+print("hello" + name)
+lastname=input("whats your lastname:") 
+print("you are " + name + lastname)
+age=int (input("how old are you:"))
+
+print ("you are "+str(age)+" years old")
